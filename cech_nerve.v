f5654968cd2b1de5f481e834ea5655dc38ceb0cd
KevinQuirin <kevin.quirin@gmail.com>
2015-03-25 09:40:28 +0100

    init

---
+++ cech_nerve.v
@@ -0,0 +1,170 @@
+Require Export Utf8_core.
+Require Import HoTT HoTT.hit.Truncations Connectedness.
+Require Import colimit.
+Require Import Peano.
+Require Import nat_lemmas.
+Require Import sub_object_classifier equivalence.
+
+Set Universe Polymorphism.
+Global Set Primitive Projections.
+(* Set Implicit Arguments. *)
+
+Local Open Scope path_scope.
+(* Local Open Scope equiv_scope. *)
+Local Open Scope type_scope.
+
+Section hProduct.
+
+  Fixpoint hProduct (Y:Type) (n:nat) : Type :=
+    match n with
+      |0 => Unit
+      |S m => Y /\ (hProduct Y m)
+    end.
+
+  Definition proj_hProduct (Y:Type) (n:nat) (P:hProduct Y n) : {p:nat & p < n} -> Y.
+    induction n.
+    - intros [p H]. apply not_lt_0 in H. destruct H.
+    - intros [p H]. simpl in P.
+      induction p.
+      + exact (fst P).
+      + apply le_pred in H. simpl in H.
+        exact (IHn (snd P) (p;H)).
+  Defined.
+
+  Definition forget_hProduct (Y:Type) (n:nat) (P:hProduct Y (S n)) : {p:nat & p <= n} -> hProduct Y n.
+    induction n.
+    - intros [p H]. exact tt.
+    - intros [p H]. simpl in P.
+      induction p.
+      + simpl. exact (fst (snd P), snd (snd P)).
+      + apply le_pred in H. simpl in H.
+        exact (fst P, (IHn (fst (snd P),snd (snd P)) (p;H))).
+  Defined.
+End hProduct.
+
+Section hPullback.
+  Context `{ua: Univalence}.
+  
+  Definition char_hPullback {X Y:Type} (f:Y -> X) (n:nat) (P : hProduct Y (S n))
+  : Type.
+    induction n.
+    - exact Unit. 
+    - simpl in P.
+      exact ((f (fst P) = f (fst (snd P))) /\ (IHn (snd P))).
+  Defined.
+  
+  Definition char_hPullback_inv  {X Y:Type} (f:Y -> X) (n:nat) (P : hProduct Y (S n))
+  : Type.
+    induction n.
+    - exact Unit. 
+    - simpl in P.
+      exact ((IHn (snd P)) /\ (f (fst P) = f (fst (snd P)))).
+  Defined.
+  
+  Definition forget_char_hPullback {X Y:Type} (f:Y -> X) (n:nat)
+             (x : hProduct Y (S (S n)))
+             (P : (char_hPullback f (S n) x))
+  : forall p:{p:nat & p <= n.+1}, (char_hPullback f n (forget_hProduct Y (S n) x p)).
+    intros [p Hp].
+    induction (decidable_paths_nat 0 p) as [| a].
+    { destruct a. simpl in *.
+      exact (snd P). }
+    
+    induction (decidable_paths_nat (S n) p) as [| b].
+    { destruct a0. simpl in *.
+      induction n.
+      simpl in *. exact tt.
+      simpl in *.
+      split. exact (fst P).
+      apply IHn. exact (snd P).
+      apply succ_not_0. }
+    
+
+    apply neq_symm in a.
+    apply neq_0_succ in a.
+    destruct a as [p' Hp'].
+    (* destruct Hp'. *)
+    assert (l := le_neq_lt _ _ (neq_symm _ _ b) Hp).
+    assert (l0 := le_pred _ _ l). simpl in l0. clear b l. simpl in *. 
+    generalize dependent n. generalize dependent p. generalize dependent p'. induction p'; intros.
+    - simpl in *.
+      destruct Hp'.   
+      assert (k := gt_0_succ n l0); destruct k as [k Hk]. destruct Hk. simpl in *.
+      split.
+      exact (fst P @ (fst (snd P))).
+      exact (snd (snd P)).
+    - simpl in *.
+      destruct Hp'.
+      assert (n' := ge_succ_succ (S p') _ l0).
+      destruct n' as [n' Hn']. destruct Hn'.  
+      specialize (IHp' (p'.+1) 1 n' (snd x) (snd P)). simpl. split.
+      exact (fst P).
+      apply IHp'.
+      exact (le_pred _ _ l0).
+  Defined.
+
+  (* Definition 7*)
+  Definition hPullback {X Y:Type} (f:Y -> X) (n:nat)
+  : Type.
+    induction n.
+    - exact Unit.
+    - exact {P : hProduct Y (S n) & (char_hPullback f n P)}.
+  Defined.
+
+  Definition proj_pullback {X Y:Type} (f:Y -> X) (n:nat) (P : hPullback f n) : forall p:{p:nat & p < n}, Y.
+    apply proj_hProduct.
+    induction n. exact P. simpl in *. exact P.1.
+  Defined.
+
+  (* This defines the canonical projections between the n+1-pullback of X to the n-pullback of X*)
+  Definition forget_hPullback {X Y:Type} (f:Y -> X) (n:nat) (P : hPullback f (S n)) : forall p:{p:nat & p <= n}, hPullback f n.
+    intros [p Hp].
+    induction n. exact tt.
+    exists (forget_hProduct Y (S n) P.1 (p;Hp)).
+    apply forget_char_hPullback.
+    exact P.2.
+  Defined.
+
+End hPullback.
+
+Section Cech_Nerve.
+  
+  Context `{ua: Univalence}.
+
+  (* Definition 8*)
+  Definition Cech_nerve_graph : graph.
+    refine (Build_graph _ _).
+    exact nat.
+    intros m n.
+    exact ((S n = m) /\ (nat_interval m)).
+  Defined.
+
+  Definition Cech_nerve_diagram {X Y:Type} (f: Y -> X) : diagram (Cech_nerve_graph).
+    refine (Build_diagram _ _ _).
+    intro n.
+    exact (hPullback f (S n)).
+    intros i j. 
+    intros [p q] a.
+    destruct p.
+    apply forget_hPullback.
+    exact a.
+    exact q.
+  Defined.
+
+  Definition Cech_nerve_commute {X Y:Type} (f: Y -> X)  
+  : forall i, (Cech_nerve_diagram f) i -> X.
+    intro i. simpl. intro P.
+    exact (f (fst P.1)).
+  Defined.
+
+  Definition Cech_nerve_pp {X Y:Type} (f: Y -> X)
+  : forall i j, forall (g : Cech_nerve_graph i j), forall (x : (Cech_nerve_diagram f) i),
+      (Cech_nerve_commute f) _ (diagram1 (Cech_nerve_diagram f) g x) = (Cech_nerve_commute f ) _ x.
+    intros i j g x. simpl in *.
+    unfold Cech_nerve_commute. simpl.
+    destruct g as [p [q Hq]]. destruct p. simpl.
+    destruct q; [exact (fst (x.2))^ | reflexivity].
+  Defined.
+
+  
+End Cech_Nerve.
---
+++ foo.v
@@ -0,0 +1,153 @@
+Require Export Utf8_core.
+Require Import HoTT HoTT.hit.Truncations Connectedness Types.Record.
+Require Import equivalence lemmas colimit cech_nerve nat_lemmas.
+
+Module Export LoopSpace.
+  
+  Private Inductive LoopSpace (X:Type@{i}) : Type@{i} :=
+|Point : LoopSpace X.
+
+  Axiom Loop : forall {X:Type}, X -> (Point X = Point X).
+
+  Definition LoopSpace_ind (X:Type) (P : LoopSpace X -> Type)
+             (H_P : P (Point X))
+             (H_Loop : forall x:X, (Loop x) # H_P = H_P)
+  : forall (y:LoopSpace X), P y
+    := fun y => (match y return (_ -> P y)
+                 with Point => (fun _ => H_P) end) H_Loop.
+
+  Axiom LoopSpace_comp_Loop : forall (X:Type) (P : LoopSpace X -> Type)
+             (H_P : P (Point X))
+             (H_Loop : forall x:X, (Loop x) # H_P = H_P)
+             (x:X),
+                                apD (LoopSpace_ind X P H_P H_Loop) (Loop x) = H_Loop x.
+
+End LoopSpace.
+
+Definition LoopSpace_rec (X Y:Type) (H_P : Y) (H_Loop : X -> H_P = H_P)
+: LoopSpace X -> Y.
+  apply (LoopSpace_ind X (λ _, Y) H_P).
+  intros x. exact (transport_const _ _ @ H_Loop x).
+Defined.
+
+Definition LoopSpace_comp_nd_Loop (X Y : Type)
+  (H_P : Y) (H_Loop : X -> H_P = H_P) (x:X)
+: ap (LoopSpace_rec X Y H_P H_Loop) (Loop x) = H_Loop x.
+Proof.
+  apply (cancelL (transport_const (Loop x) H_P)).
+  transitivity (apD (LoopSpace_rec X Y H_P H_Loop) (Loop x)).
+  symmetry; refine (apD_const (LoopSpace_rec X Y H_P H_Loop) _).
+  refine (LoopSpace_comp_Loop X (fun _ : LoopSpace X => Y) H_P _ x).
+Defined.
+
+Definition LoopSpace_comp (X Y:Type) (H_P : Y) (H_Loop : X -> H_P = H_P)
+: LoopSpace_rec X Y H_P H_Loop (Point X) = H_P.
+  reflexivity.
+Defined.
+
+Definition LoopSpace_eta `{fs : Funext} (X:Type) (P:LoopSpace X -> Type) (f:forall y, P y)
+: f = LoopSpace_ind X P (f (Point X)) (λ x, apD f (Loop x)).
+  apply path_forall.
+  unfold pointwise_paths.
+  refine (LoopSpace_ind X
+                        (λ x, f x = LoopSpace_ind X P (f (Point X)) (λ x0 : X, apD f (Loop x0)) x)
+                        idpath _).
+  intro x.
+  refine (transport_paths_FlFr_D
+    (g := LoopSpace_ind X P (f (Point X)) (λ x, apD f (Loop x)))
+    _ _ @ _); simpl.
+  apply moveR_pM. apply (concat (concat_p1 _)), (concatR (concat_1p _)^).
+  apply ap, inverse. refine (LoopSpace_comp_Loop _ _ _ _ _).
+Defined.
+
+Section Foo.
+
+  Context `{ua : Univalence}.
+  Context `{fs : Funext}.
+
+  Definition Unit_to_BX (X:Type) (BX := LoopSpace X) : Unit -> BX
+    := λ tt, Point X.
+
+  Definition CN_BX (X:Type) (BX := LoopSpace X) := Cech_nerve_diagram (Unit_to_BX X).
+
+  Definition cone_morphism (G:graph) (D: diagram G) (X:Type) (q r: forall i, D i -> X) (pp_q : forall (i j:G) (f: G i j) (x: D i), q _ (diagram1 D f x) = q _ x) (pp_r : forall (i j:G) (f: G i j) (x: D i), r _ (diagram1 D f x) = r _ x)
+             (eq_qr : forall i, q i == r i)
+             (eq_pp_qr : forall i j f x, pp_q i j f x @ eq_qr i x = eq_qr j (diagram1 D f x) @ pp_r i j f x)
+  : (existT (λ qq : forall i, D i -> X, forall (i j:G) (f: G i j) (x: D i), qq j (diagram1 D f x) = qq i x) q pp_q) = (r;pp_r).
+    refine (path_sigma' _ (path_forall _ _ (λ i, path_forall _ _ (eq_qr i))) _). simpl.
+    apply path_forall; intro i.
+    apply path_forall; intro j.
+    apply path_forall; intro f.
+    apply path_forall; intro x.
+
+    repeat rewrite transport_forall_constant.
+    rewrite transport_paths_FlFr. simpl.
+    rewrite concat_pp_p. apply moveR_Vp. simpl.
+    rewrite (ap_ap2_path_forall (λ u, D u) (λ _, λ _, X) q r eq_qr i x).
+    rewrite (ap_ap2_path_forall (λ u, D u) (λ _, λ _, X) q r eq_qr j (diagram1 D f x)).
+    apply eq_pp_qr.
+  Qed.
+
+  Definition colimit_unicity (G:graph) (D: diagram G) (X Y:Type) (q : forall i, D i -> X) (r : forall i, D i -> Y) (pp_q : forall (i j:G) (f: G i j) (x: D i), q _ (diagram1 D f x) = q _ x) (pp_r : forall (i j:G) (f: G i j) (x: D i), r _ (diagram1 D f x) = r _ x)
+             (colimX : is_colimit G D X q pp_q)
+             (colimY : is_colimit G D Y r pp_r)
+  : X <~> Y.
+    refine (equiv_adjointify _ _ _ _).
+    - exact (equiv_inv (IsEquiv := (colimX Y)) (r;pp_r)).
+    - exact (equiv_inv (IsEquiv := (colimY X)) (q;pp_q)).
+    - intro x.
+      
+  
+  Lemma BX_colimit_CN_BX (G:Type) (BG := LoopSpace G)
+  : is_colimit (Cech_nerve_graph)
+               (CN_BX G)
+               BG
+               (Cech_nerve_commute (Unit_to_BX G))
+               (Cech_nerve_pp (Unit_to_BX G)).
+    intro X.
+    refine (isequiv_adjointify _ _ _ _).
+    - intros [qq p_qq].
+      refine (LoopSpace_rec G X _ _).
+      apply (qq 0).
+      simpl. exact ((tt,tt);tt).
+      intro x.
+      pose (e1 := p_qq 1 0 (idpath,(1;le_n 1)) ((tt,(tt,tt));(Loop x, tt))).
+      pose (e2 := p_qq 1 0 (idpath,(0;(le_S _ _ (le_n 0)))) ((tt,(tt,tt));(Loop x, tt))).
+      exact (e1 @ e2^).
+      (* reflexivity. *)
+    - intros [qq p_qq].
+
+      refine (cone_morphism _ _ _ _ _ _ _ _ _).
+      + intros i x.  simpl.
+      induction i.
+      destruct x as [[x1 x2] x3]; destruct x1, x2, x3.
+      reflexivity.
+      exact ((IHi (snd x.1; snd x.2)) @ (p_qq i.+1 i (idpath,(0;le_0 (i.+1))) x)).
+      + intros i j [f [q Hq]] x. destruct f; simpl.
+
+        match goal with
+          |[|- ap _ ?pp @ _ = _ ] => set (foo := pp)
+        end. simpl in foo.
+        unfold Unit_to_BX in foo; simpl in foo.
+        induction j. simpl. simpl in x.
+
+        destruct (le_1_is_01 q Hq).
+        symmetry in p; destruct p. simpl.
+        admit.
+
+        
+        admit.
+        admit.
+
+    
+    - intro φ.
+      simpl. unfold Cech_nerve_commute, Unit_to_BX, LoopSpace_rec.
+      pose (LoopSpace_eta G (λ _ : LoopSpace G, X) φ)^.
+      path_via (LoopSpace_ind G (λ _ : LoopSpace G, X) (φ (Point G))
+                              (λ x : G, apD φ (Loop x))).
+      apply ap. apply path_forall; intro x.
+      rewrite apD_const. hott_simpl.
+      
+  Qed.
+      
+End Foo.
