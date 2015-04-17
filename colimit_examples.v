c8e9debe88cd55dae4d8df1c777d7e7ca028e943
SimonBoulier <simon.boulier@ens-rennes.fr>
2015-04-17 15:41:08 +0200

    examples

--- colimit_examples.v
+++ colimit_examples.v
@@ -1,21 +1,105 @@
 Require Export Utf8_core.
 Require Import HoTT.
-Require Import equivalence lemmas colimit.
+Require Import MyTacs equivalence lemmas colimit.
 
-Set Universe Polymorphism.
-Global Set Primitive Projections.
+Context `{fs : Funext}.
 
-Local Open Scope path_scope.
+
+Section Coproduct.
+
+  Definition coproduct_graph : graph.
+    refine (Build_graph _ _).
+    - exact Bool.
+    - intros A B; exact Empty.
+  Defined.
+
+  Definition coproduct_diag (A B: Type) : diagram coproduct_graph.
+    refine (Build_diagram _ _ _).
+    - intro x; destruct x.
+      exact A. exact B.
+    - intros i j; destruct i, j; intro H; destruct H.
+  Defined.
+  
+  Definition coproduct_cocone (A B: Type)
+  : cocone (coproduct_diag A B) (A + B).
+    refine (exist _ _ _).
+    - intros i x; destruct i. exact (inl x). exact (inr x).
+    - intros i j f x; destruct i, j, f.
+  Defined.
+
+  Lemma is_coproduct_coproduct (A B: Type)
+  : is_colimit _ _ (coproduct_cocone A B).
+    intros X.
+    refine (isequiv_adjointify _ _ _ _).
+    - intros C x. destruct x. exact (C.1 true a). exact (C.1 false b).
+    - intros C. refine (path_cocone _ _ _ _ _ _ _).
+      + intros i x; destruct i; reflexivity.
+      + intros i j f x; destruct i, j, f.
+    - intros F. funext x; destruct x; reflexivity.
+  Defined.
+
+End Coproduct.
+
+
+
+Section Coequalizer.
+
+  Definition coequalizer_graph : graph.
+    refine (Build_graph _ _).
+    - exact Bool.
+    - intros i j; exact (if i then if j then Empty else Bool else Empty).
+  Defined.
+
+  Definition coequalizer_diag {B A: Type} (f g: B -> A) : diagram coequalizer_graph.
+    refine (Build_diagram _ _ _).
+    - intro x; destruct x.
+      exact B. exact A.
+    - intros i j; destruct i, j; intro H; destruct H. exact f. exact g.
+  Defined.
+  
+  Definition coequalizer_cocone {B A: Type} (f g: B -> A)
+  : cocone (coequalizer_diag f g) (Coeq f g).
+    refine (exist _ _ _).
+    - intros i x; destruct i; simpl in *. exact (coeq (g x)). exact (coeq x).
+    - intros i j φ x; destruct i, j, φ; simpl. exact (cp x). reflexivity.
+  Defined.
+
+  Lemma is_coequalizer_coequalizer {B A: Type} (f g: B -> A)
+  : is_colimit _ _ (coequalizer_cocone f g).
+    intros X.
+    refine (isequiv_adjointify _ _ _ _).
+    - intros C. refine (Coeq_rec _ _ _). exact (C.1 false).
+      intros b. etransitivity.
+      exact (C.2 true false true b).
+      exact (C.2 true false false b)^.
+    - intros C. refine (path_cocone _ _ _ _ _ _ _).
+      + intros i x; destruct i; simpl. exact (C.2 true false false x). reflexivity.
+      + intros i j φ x; destruct i, j, φ; simpl.
+        * hott_simpl.
+          match goal with
+            | [|- ap (Coeq_rec ?a ?b ?c) _ @ _ = _ ] => rewrite (Coeq_rec_beta_cp a b c)
+          end. hott_simpl.
+        * reflexivity.
+    - intros F. apply path_forall.
+      match goal with
+        | [|- ?G == _ ] => refine (Coeq_ind (λ w, G w = F w) _ _)
+      end.
+      + simpl. reflexivity.
+      + intros b. simpl.
+        rewrite transport_paths_FlFr.
+        rewrite Coeq_rec_beta_cp. hott_simpl.
+  Defined.
+
+End Coequalizer.
+
+
 
 Section Pushout.
-  
-  Context `{fs : Funext}.
 
   Inductive Pushout_graph_shape :=
     |Pushout_Corner : Pushout_graph_shape
-    |Pushout_Left : Pushout_graph_shape
+    |Pushout_Right : Pushout_graph_shape
     |Pushout_Down : Pushout_graph_shape.
- 
   
   Definition pushout_graph : graph.
     refine (Build_graph _ _).
@@ -37,26 +121,46 @@ Section Pushout.
       exact f. exact g.
   Defined.
 
-End Pushout.
-
-Section Coequalizers.
-
-  Inductive coeq_graph_shape :=
-|Coeq_Left : coeq_graph_shape
-|Coeq_Right : coeq_graph_shape.
-
-  Definition coeq_graph : graph.
-    refine (Build_graph _ _).
-    - exact coeq_graph_shape.
-    - intros i j. induction i.
-      induction j. exact Empty. exact Bool. exact Empty.
+  Definition pushout_cocone_build {A B C:Type} (f:A -> B) (g:A -> C) {P: Type} (pi1: B -> P) (pi2: C -> P)
+             (H: pi1 o f == pi2 o g)
+  : cocone (pushout_diag f g) P.
+    refine (exist _ _ _).
+    - induction i; simpl. exact (pi1 o f). exact pi1. exact pi2.
+    - induction i; induction j; induction f0; simpl. reflexivity. intros a. exact (H a)^.
   Defined.
 
-  Definition coeq_diagram {A B:Type} (f g:A -> B) : diagram coeq_graph.
-    refine (Build_diagram _ _ _).
-    - intro i; induction i. exact A. exact B.
-    - intros i j; induction i, j; try (intro H; destruct H); simpl.
-      exact f. exact g.
+  Definition pushout_cocone {A B C:Type} (f:A -> B) (g:A -> C)
+  : cocone (pushout_diag f g) (pushout f g).
+    refine (pushout_cocone_build f g _ _ _).
+    exact (push o inl). exact (push o inr). exact Pushout.pp.
+  Defined.
+
+  Lemma is_pushout_pushout {A B C:Type} (f:A -> B) (g:A -> C)
+  : is_colimit _ _ (pushout_cocone f g).
+    intros X. refine (isequiv_adjointify _ _ _ _).
+    - intros Co. refine (pushout_rec _ _ _).
+      + intros x; induction x. exact (Co.1 Pushout_Right a). exact (Co.1 Pushout_Down b).
+      + intros a. simpl. etransitivity. exact (Co.2 Pushout_Corner Pushout_Right tt a).
+        exact (Co.2 Pushout_Corner Pushout_Down tt a)^.
+    - intros Co.
+      refine (path_cocone _ _ _ _ _ _ _).
+      + induction i; intros x; simpl; try reflexivity. exact (Co.2 Pushout_Corner Pushout_Right tt x).
+      + induction i, j, f0; simpl. reflexivity.
+        intros x. hott_simpl. rewrite <- inverse_ap.
+        match goal with
+          | [|-  (ap (pushout_rec ?a ?b ?c) _)^ @ _ = _ ] => rewrite (pushout_rec_beta_pp a b c x)
+        end. rewrite inv_pp. hott_simpl.
+    - intros F. funext x. simpl.
+      match goal with
+        | [|- pushout_rec _ ?pu ?pp _ = _ ] => set (push' := pu); set (pp' := λ a : A, ap F (Pushout.pp a)); transitivity (pushout_rec _ push' pp' x)
+      end.
+      apply ap10. apply ap. subst pp'.
+      funext a. hott_simpl.
+      refine (pushout_ind f g (λ x, pushout_rec X push' pp' x = F x) _ _ x).
+      + induction a; reflexivity.
+      + intros a; simpl.
+        rewrite transport_paths_FlFr.
+        rewrite pushout_rec_beta_pp. hott_simpl.
   Defined.
   
-End Coequalizers.
+End Pushout.
