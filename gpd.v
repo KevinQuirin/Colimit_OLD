73006da1ca67591fa24a52480e02aec713fd0674
KevinQuirin <kevin.quirin@gmail.com>
2015-07-02 12:18:42 +0200

    VD truncation

--- gpd.v
+++ gpd.v
@@ -38,101 +38,102 @@ Section Gpd.
 
 End Gpd.
 
-Section Colimit_Gpd.
+Section Colimit_truncation.
 
-  
-  (* Lemma transport_hfiber {X Y:Type} (f:X -> Y) (y z:Y) (a:hfiber f y) (b:hfiber f z) (p:y=z) (q:p # a = b) *)
-  (* : transport (λ u:Y, hfiber f u) p a = b. *)
-  (* Proof. *)
-  (*   pose (@transport_sigma' Y X (λ y x, f x = y) y z p a); simpl in p0. *)
-    
-  (*   destruct p. simpl. exact q. Defined. *)
-  
-  Local Notation T f i := (Gpd_aux f i).1.
-  Local Notation α f i := (Gpd_aux f i).2.
+  Require Import VD_truncation.
+  Variable X:Type.
+  Let D := @Gpd X Unit (λ x, tt).
+  Let Q := colimit D.
+  Let D' := @Gpd_ X.
 
-  Variables X Y:Type. Variable f:X -> Y.
-  Local Notation Tf i := (T f i).
-  Local Notation αf i := (α f i).
-
-  Lemma bar (T:Type) (A B: T -> Type) (g h:forall y:T, A y -> B y) (y z:T) (p:y=z)
-    : forall (b:B y) (b':B z) (q: p # b = b'), transport (λ u, Coeq (g u) (h u)) p (@coeq _ _ (g y) (h y) b) = @coeq _ _ (g z) (h z) b'.
+  Lemma PB_unit_is_prod (A B: Type)
+    : Pullback (λ x:A, tt) (λ x:B, tt) <~> A*B.
   Proof.
-    intros b b'.
-    destruct p. simpl.
-    exact (ap coeq).
+    refine (equiv_adjointify _ _ _ _).
+    - intros [a [b p]]; exact (a,b).
+    - intros [a b]; exact (a;(b;path_ishprop _ _)).
+    - intros [a b]; reflexivity.
+    - intros [a [b p]]; apply path_sigma' with 1; apply path_sigma' with 1.
+      apply path_ishprop.
   Defined.
 
-  
-  Lemma foo : {y:Y & T (λ x:hfiber f y, tt) 1} <~> T f 1.
+  Lemma Gpd_aux_is_tt : forall i, (Gpd_aux (λ _ : X, tt) i).2 = λ x, tt.
   Proof.
-    assert (X0 : (∃ y : Y, T (λ _ : hfiber f y, tt) 1) <~> (∃ y : Y, Coeq (fst : (hfiber f y)*(hfiber f y) -> _) snd)).
-    refine (equiv_functor_sigma_id _).
-    intro a. simpl.
-    refine (equiv_adjointify _ _ _ _).
-    refine (functor_coeq _ _ _ _).
-    exact (λ x, (x.1,x.2.1)).
-    exact idmap.
-    intro; reflexivity.
-    intro; reflexivity.
-    refine (functor_coeq _ _ _ _).
-    exact (λ x, (fst x; (snd x; 1))).
-    exact idmap.
-    intro; reflexivity.
-    intro; reflexivity.
-    refine (functor_coeq_sect _ _ _ _ _ _ _ _ _ _ _ _).
-    intro; reflexivity.
-    intro; reflexivity.
-    intro; reflexivity.
-    intro; reflexivity.
-    refine (functor_coeq_sect _ _ _ _ _ _ _ _ _ _ _ _).
-    intro x; simpl.
-    apply path_sigma' with 1. simpl. apply path_sigma' with 1. simpl. apply path_ishprop.
-    intro; reflexivity.
-    intros [b [c p]]. simpl. rewrite concat_1p.
-    assert (d : 1 = p) by apply path_ishprop; destruct d.
-    match goal with
-    |[|- ap _ (_ _ _ (_ _ _ ?xx)) = _ ] => assert (rew : 1 = xx) by apply path_ishprop; destruct rew
-    end.
+    intro i. apply path_forall; intro x.
+    apply path_ishprop.
+  Defined.
+  
+  Lemma Gpd_aux_is_Gpd_aux_ : forall i, (Gpd_aux (λ _ : X, tt) i).1 = Gpd_aux_ X i.
+  Proof.
+    induction i.
     reflexivity.
-
-    intros [b [c p]]. simpl. rewrite concat_1p.
-    assert (d : 1 = p) by apply path_ishprop; destruct d.
-    match goal with
-    |[|- ap _ (_ _ _ (_ _ _ ?xx)) = _ ] => assert (rew : 1 = xx) by apply path_ishprop; destruct rew
-    end.
-    reflexivity.
-    
-    etransitivity; [exact X0 | idtac]; clear X0.
-    
+    simpl.
+    apply path_universe_uncurried.
+    refine (equiv_functor_coeq' _ _ _ _).
     refine (equiv_adjointify _ _ _ _).
-    - intros [y p]; simpl in *.
-      revert p. refine (functor_coeq _ _ _ _).
-      intros p. exists (fst p).1. exists (snd p).1.
-      exact ((fst p).2 @ (snd p).2^).
-      exact pr1.
-      intro; reflexivity.
-      intro; reflexivity.
-    - refine (Coeq_rec _ _ _).
-      simpl. intro x.
-      exact (f x; coeq (x;1)).
-      intros p; simpl in *.
-      apply path_sigma' with p.2.2.     
-      etransitivity; [idtac | exact (@cp _ _ (fst : (hfiber f (f p.2.1))*(hfiber f (f p.2.1)) -> _) snd ((p.1;p.2.2), (p.2.1;1)))]. cbn.
+    intro x; cbn in *.
+    apply (transport (λ U:Type, U*U) IHi).
+    exact (pullback_pr1 x, pullback_pr2 x).
+    
+    intro x.
+    exists (transport (λ U, U) IHi^ (fst x)).
+    exists (transport (λ U, U) IHi^ (snd x)).
+    apply path_ishprop.
+    
+    abstract (
+        intro x;
+        rewrite transport_prod; cbn; repeat rewrite transport_pV; reflexivity
+      ).
+    
+    abstract (
+        intro x; refine (path_sigma' _ _ _);
+        [rewrite transport_prod; simpl; rewrite transport_Vp; reflexivity |
+         rewrite transport_sigma; simpl;
+         refine (path_sigma' _ _ _);
+         [rewrite transport_const; rewrite transport_prod; simpl;
+          rewrite transport_Vp; reflexivity |
+          apply path_ishprop] ]
+      ).
 
-      apply (@bar Y (λ y, (hfiber f y)*(hfiber f y)) (λ y, hfiber f y) (λ _, fst) (λ _, snd) (f p.1) (f p.2.1) p.2.2 (p.1;1) (p.1;p.2.2)).
-      abstract (
-          pose (@transport_sigma' Y X (λ y x, f x = y) (f p.1) (f p.2.1) p.2.2 (p.1;1)); simpl in p0;
-          rewrite transport_paths_FlFr in p0; simpl in p0;
-          rewrite ap_const in p0; rewrite ap_idmap in p0;
-          simpl in p0; rewrite concat_1p in p0; exact p0 ).
+    apply equiv_path; exact IHi.
+    abstract (intro x; simpl; rewrite transport_prod; reflexivity ).
+    abstract (intro x; simpl; rewrite transport_prod; reflexivity ).
+  Defined.
+
+    
+  Theorem Gpd_is_Gpd_ : Q <~> (Trunc (-1) X).
+  Proof.
+    assert (H : D = D').
+    {
+      apply path_diagram.
+      refine (exist _ _ _).
       
-    - cbn.
-      refine (Coeq_ind _ _ _).
-      intro a. reflexivity.
-      cbn. intros [a [b p]].
-      rewrite transport_paths_FlFr.
-      rewrite ap_idmap.
-      cbn.
+      apply path_forall; intro i. apply Gpd_aux_is_Gpd_aux_.
+      intros i j x. destruct x. simpl.
+      intro x.
+      unfold ap10, path_forall. repeat rewrite eisretr.
+      apply (moveL_transport_V idmap).
+      simpl.
+      rewrite transport_path_universe_uncurried.
+      reflexivity. }
+    unfold Q.
+    
+    etransitivity; try exact (VD_truncation X). unfold D' in H; destruct H.
+    exact equiv_idmap.
+  Defined.
+    
+End Colimit_truncation.
 
+Section Colimit_Gpd.
+
+  Definition Gpd_cocone (X Y:Type) (f:X -> Y)
+  : cocone (Gpd f) Y.
+  Proof.
+    refine (exist _ _ _).
+    - intro i. simpl. exact (Gpd_aux f i).2.
+    - intros i j f0 x. destruct f0. reflexivity.
+  Defined.
+  
+  Axiom VD_Giraud : forall (X Y:Type), forall (f:X -> Y), IsSurjection f -> (is_colimit (Gpd_cocone f)).
+                                                                               
 End Colimit_Gpd.
